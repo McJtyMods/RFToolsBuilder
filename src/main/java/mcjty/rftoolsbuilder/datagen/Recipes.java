0ef29f9eec0363d7beda01c897ea381593ad57d0
McJty <mcjty1@gmail.com>
2021-07-07 14:41:07 +0200

    More porting work on the environmental controller

--- src/main/java/mcjty/rftoolsbuilder/datagen/Recipes.java
+++ src/main/java/mcjty/rftoolsbuilder/datagen/Recipes.java
@@ -38,17 +38,17 @@ public class Recipes extends BaseRecipeProvider {
                         .unlockedBy("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
                 "gTg", "rFr", "OOO");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ShieldModule.SHIELD_BLOCK2.get())
-                        .key('M', ShieldModule.SHIELD_BLOCK1.get())
-                        .addCriterion("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
+                        .define('M', ShieldModule.SHIELD_BLOCK1.get())
+                        .unlockedBy("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
                 "ROR", "OMO", "ROR");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ShieldModule.SHIELD_BLOCK3.get())
-                        .key('M', ShieldModule.SHIELD_BLOCK2.get())
-                        .addCriterion("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
+                        .define('M', ShieldModule.SHIELD_BLOCK2.get())
+                        .unlockedBy("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
                 "sOs", "OMO", "sOs");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ShieldModule.SHIELD_BLOCK4.get())
-                        .key('M', ShieldModule.SHIELD_BLOCK3.get())
-                        .key('n', Items.NETHER_STAR)
-                        .addCriterion("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
+                        .define('M', ShieldModule.SHIELD_BLOCK3.get())
+                        .define('n', Items.NETHER_STAR)
+                        .unlockedBy("machine_frame", has(VariousModule.MACHINE_FRAME.get())),
                 "nOs", "OMO", "sOn");
 
         build(consumer, ShapedRecipeBuilder.shaped(ShieldModule.TEMPLATE_BLUE.get(), 8)
@@ -73,65 +73,65 @@ public class Recipes extends BaseRecipeProvider {
                 "www", "lGl", "www");
 
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_PUMP.get())
-                        .key('M', BuilderModule.SHAPE_CARD_DEF.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_DEF.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "rWr", "bMb", "rLr");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_PUMP_CLEAR.get())
-                        .key('M', BuilderModule.SHAPE_CARD_PUMP.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_PUMP.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "GGG", "GMG", "GGG");
         build(consumer, new ResourceLocation(RFToolsBuilder.MODID, "shape_card_pump_dirt"), CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_PUMP.get())
-                        .key('M', BuilderModule.SHAPE_CARD_PUMP_CLEAR.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_PUMP_CLEAR.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "DDD", "DMD", "DDD");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_LIQUID.get())
-                        .key('M', BuilderModule.SHAPE_CARD_DEF.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_DEF.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "rWr", "iMi", "rLr");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY.get())
-                        .key('P', Items.DIAMOND_PICKAXE)
-                        .key('S', Items.DIAMOND_SHOVEL)
-                        .key('M', BuilderModule.SHAPE_CARD_DEF.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('P', Items.DIAMOND_PICKAXE)
+                        .define('S', Items.DIAMOND_SHOVEL)
+                        .define('M', BuilderModule.SHAPE_CARD_DEF.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "rPr", "iMi", "rSr");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_CLEAR.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "GGG", "GMG", "GGG");
         build(consumer, new ResourceLocation(RFToolsBuilder.MODID, "shape_card_quarry_dirt"), CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "DDD", "DMD", "DDD");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_FORTUNE.get())
-                        .key('n', Items.GHAST_TEAR)
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('n', Items.GHAST_TEAR)
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "sns", "eMd", "srs");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_CLEAR_FORTUNE.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY_FORTUNE.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY_FORTUNE.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "GGG", "GMG", "GGG");
         build(consumer, new ResourceLocation(RFToolsBuilder.MODID, "shape_card_quarry_fortune_dirt"), CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_FORTUNE.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR_FORTUNE.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR_FORTUNE.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "DDD", "DMD", "DDD");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_SILK.get())
-                        .key('n', Items.NETHER_STAR)
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('n', Items.NETHER_STAR)
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "sns", "dMd", "sds");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_CLEAR_SILK.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY_SILK.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY_SILK.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "GGG", "GMG", "GGG");
         build(consumer, new ResourceLocation(RFToolsBuilder.MODID, "shape_card_quarry_silk_dirt"), CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_QUARRY_SILK.get())
-                        .key('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR_SILK.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('M', BuilderModule.SHAPE_CARD_QUARRY_CLEAR_SILK.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "DDD", "DMD", "DDD");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(BuilderModule.SHAPE_CARD_VOID.get())
-                        .key('x', Tags.Items.DYES_BLACK)
-                        .key('M', BuilderModule.SHAPE_CARD_DEF.get())
-                        .addCriterion("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
+                        .define('x', Tags.Items.DYES_BLACK)
+                        .define('M', BuilderModule.SHAPE_CARD_DEF.get())
+                        .unlockedBy("shape_card", has(BuilderModule.SHAPE_CARD_DEF.get())),
                 "xOx", "OMO", "xOx");
 
         build(consumer, ShapedRecipeBuilder.shaped(BuilderModule.SPACE_CHAMBER_CARD.get(), 1)
