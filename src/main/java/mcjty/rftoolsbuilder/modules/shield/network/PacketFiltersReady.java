00ccb083f5cc09f660f62517b71acee9c77a6d59
McJty <mcjty1@gmail.com>
2021-11-18 06:51:11 +0100

    Work on a better list command system. WIP

--- src/main/java/mcjty/rftoolsbuilder/modules/shield/network/PacketFiltersReady.java
+++ src/main/java/mcjty/rftoolsbuilder/modules/shield/network/PacketFiltersReady.java
@@ -1,66 +1,30 @@
 package mcjty.rftoolsbuilder.modules.shield.network;
 
-import mcjty.lib.tileentity.GenericTileEntity;
+import mcjty.lib.network.AbstractPacketSendResultToClient;
 import mcjty.rftoolsbuilder.modules.shield.filters.AbstractShieldFilter;
 import mcjty.rftoolsbuilder.modules.shield.filters.ShieldFilter;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fml.network.NetworkEvent;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 
-public class PacketFiltersReady {
-
-    private BlockPos pos;
-    private List<ShieldFilter> list;
-    private String command;
-
-    public PacketFiltersReady() {
-    }
+public class PacketFiltersReady extends AbstractPacketSendResultToClient<ShieldFilter> {
 
     public PacketFiltersReady(PacketBuffer buf) {
-        pos = buf.readBlockPos();
-        command = buf.readUtf(32767);
-        int size = buf.readInt();
-        if (size != -1) {
-            list = new ArrayList<>(size);
-            for (int i = 0 ; i < size ; i++) {
-                ShieldFilter item = AbstractShieldFilter.createFilter(buf);
-                list.add(item);
-            }
-        } else {
-            list = null;
-        }
+        super(buf);
     }
 
     public PacketFiltersReady(BlockPos pos, String command, List<ShieldFilter> list) {
-        this.pos = pos;
-        this.command = command;
-        this.list = new ArrayList<>();
-        this.list.addAll(list);
+        super(pos, command, list);
     }
 
-    public void toBytes(PacketBuffer buf) {
-        buf.writeBlockPos(pos);
-        buf.writeUtf(command);
-
-        if (list == null) {
-            buf.writeInt(-1);
-        } else {
-            buf.writeInt(list.size());
-            for (ShieldFilter item : list) {
-                item.toBytes(buf);
-            }
-        }
+    @Override
+    protected ShieldFilter readElement(PacketBuffer buf) {
+        return AbstractShieldFilter.createFilter(buf);
     }
 
-    public void handle(Supplier<NetworkEvent.Context> supplier) {
-        NetworkEvent.Context ctx = supplier.get();
-        ctx.enqueueWork(() -> {
-            GenericTileEntity.executeClientCommandHelper(pos, command, list);
-        });
-        ctx.setPacketHandled(true);
+    @Override
+    protected void writeElement(PacketBuffer buf, ShieldFilter element) {
+        element.toBytes(buf);
     }
 }
--- src/main/java/mcjty/rftoolsbuilder/modules/shield/network/PacketGetFilters.java
+++ src/main/java/mcjty/rftoolsbuilder/modules/shield/network/PacketGetFilters.java
@@ -1,61 +1,39 @@
 package mcjty.rftoolsbuilder.modules.shield.network;
 
-import mcjty.lib.network.TypedMapTools;
-import mcjty.lib.tileentity.GenericTileEntity;
+import mcjty.lib.network.AbstractPacketGetListFromServer;
 import mcjty.lib.typed.TypedMap;
-import mcjty.lib.varia.Logging;
 import mcjty.rftoolsbuilder.modules.shield.blocks.ShieldProjectorTileEntity;
 import mcjty.rftoolsbuilder.modules.shield.filters.ShieldFilter;
 import mcjty.rftoolsbuilder.setup.RFToolsBuilderMessages;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.network.NetworkDirection;
-import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 import java.util.List;
-import java.util.function.Supplier;
 
-public class PacketGetFilters {
-
-    protected BlockPos pos;
-    protected TypedMap params;
-
-    public PacketGetFilters() {
-    }
+public class PacketGetFilters extends AbstractPacketGetListFromServer<ShieldFilter> {
 
     public PacketGetFilters(PacketBuffer buf) {
-        pos = buf.readBlockPos();
-        params = TypedMapTools.readArguments(buf);
+        super(buf);
     }
 
     public PacketGetFilters(BlockPos pos) {
-        this.pos = pos;
-        this.params = TypedMap.EMPTY;
+        super(pos, ShieldProjectorTileEntity.CMD_GETFILTERS.getName(), TypedMap.EMPTY);
     }
 
-    public void toBytes(PacketBuffer buf) {
-        buf.writeBlockPos(pos);
-        TypedMapTools.writeArguments(buf, params);
+    @Override
+    protected SimpleChannel getChannel() {
+        return RFToolsBuilderMessages.INSTANCE;
     }
 
-    public void handle(Supplier<NetworkEvent.Context> supplier) {
-        NetworkEvent.Context ctx = supplier.get();
-        ctx.enqueueWork(() -> {
-            World world = ctx.getSender().getCommandSenderWorld();
-            if (world.hasChunkAt(pos)) {
-                TileEntity te = world.getBlockEntity(pos);
-                if (te instanceof GenericTileEntity) {
-                    List<ShieldFilter> list = ((GenericTileEntity) te).executeServerCommandList(ShieldProjectorTileEntity.CMD_GETFILTERS.getName(), ctx.getSender(), params, ShieldFilter.class);
-                    RFToolsBuilderMessages.INSTANCE.sendTo(new PacketFiltersReady(pos, ShieldProjectorTileEntity.CMD_GETFILTERS.getName(), list),
-                            ctx.getSender().connection.connection, NetworkDirection.PLAY_TO_CLIENT);
-                } else {
-                    Logging.log("Command not handled!");
-                    return;
-                }
-            }
-        });
-        ctx.setPacketHandled(true);
+    @Override
+    protected Class<ShieldFilter> getType() {
+        return ShieldFilter.class;
     }
+
+    @Override
+    protected Object createReturnPacket(List<ShieldFilter> list) {
+        return new PacketFiltersReady(pos, command, list);
+    }
+
 }
