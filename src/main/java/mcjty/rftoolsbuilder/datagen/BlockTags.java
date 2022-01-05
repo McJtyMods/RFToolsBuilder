fa1c3e4d445dc1c56179fb0f2e4d1f886140c5cb
McJty <mcjty1@gmail.com>
2022-01-05 15:43:35 +0100

    Datagen for tags

---
+++ src/main/java/mcjty/rftoolsbuilder/datagen/BlockTags.java
@@ -0,0 +1,33 @@
+package mcjty.rftoolsbuilder.datagen;
+
+import mcjty.lib.datagen.BaseBlockTagsProvider;
+import mcjty.rftoolsbuilder.RFToolsBuilder;
+import mcjty.rftoolsbuilder.modules.builder.BuilderModule;
+import mcjty.rftoolsbuilder.modules.scanner.ScannerModule;
+import mcjty.rftoolsbuilder.modules.shield.ShieldModule;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nonnull;
+
+public class BlockTags extends BaseBlockTagsProvider {
+
+    public BlockTags(DataGenerator generator, ExistingFileHelper helper) {
+        super(generator, RFToolsBuilder.MODID, helper);
+    }
+
+    @Override
+    protected void addTags() {
+        ironPickaxe(
+                BuilderModule.BUILDER, BuilderModule.SPACE_CHAMBER, BuilderModule.SPACE_CHAMBER_CONTROLLER, BuilderModule.SUPPORT,
+                ShieldModule.SHIELD_BLOCK1, ShieldModule.SHIELD_BLOCK2, ShieldModule.SHIELD_BLOCK3, ShieldModule.SHIELD_BLOCK4,
+                ShieldModule.SHIELDING_CUTOUT, ShieldModule.SHIELDING_SOLID, ShieldModule.SHIELDING_TRANSLUCENT
+        );
+    }
+
+    @Override
+    @Nonnull
+    public String getName() {
+        return "RFToolsBuilder Tags";
+    }
+}
