775afbd02b18bab3db0ea654e14b34ba7b8fd3ba
McJty <mcjty1@gmail.com>
2022-12-28 16:25:03 +0100

    Forgot

---
+++ src/main/java/mcjty/rftoolsbuilder/compat/rftoolsutility/RFToolsSupport.java
@@ -0,0 +1,19 @@
+package mcjty.rftoolsbuilder.compat.rftoolsutility;
+
+import mcjty.rftoolsbase.api.screens.IScreenModuleRegistry;
+import mcjty.rftoolsbuilder.modules.mover.items.VehicleControlScreenModule;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+
+public class RFToolsSupport {
+
+    public static class GetScreenModuleRegistry implements Function<IScreenModuleRegistry, Void> {
+        @Nullable
+        @Override
+        public Void apply(IScreenModuleRegistry manager) {
+            manager.registerModuleDataFactory(VehicleControlScreenModule.EmptyData.ID, VehicleControlScreenModule.EmptyData::new);
+            return null;
+        }
+    }
+}
