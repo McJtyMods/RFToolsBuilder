e5b82e930c8899efc85f3957ba656ac1a83e797a
McJty <mcjty1@gmail.com>
2023-06-19 21:18:33 +0200

    The creative tabs work

--- src/main/java/mcjty/rftoolsbuilder/setup/ModSetup.java
+++ src/main/java/mcjty/rftoolsbuilder/setup/ModSetup.java
@@ -2,18 +2,11 @@ package mcjty.rftoolsbuilder.setup;
 
 import mcjty.lib.compat.MainCompatHandler;
 import mcjty.lib.setup.DefaultModSetup;
-import mcjty.rftoolsbuilder.RFToolsBuilder;
 import mcjty.rftoolsbuilder.compat.rftoolsutility.RFToolsSupport;
-import mcjty.rftoolsbuilder.modules.builder.BuilderModule;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class ModSetup extends DefaultModSetup {
-
-    public ModSetup() {
-        createTab(RFToolsBuilder.MODID, "rftoolsbuilder", () -> new ItemStack(BuilderModule.BUILDER.get()));
-    }
 
     @Override
     public void init(FMLCommonSetupEvent e) {
--- src/main/java/mcjty/rftoolsbuilder/setup/Registration.java
+++ src/main/java/mcjty/rftoolsbuilder/setup/Registration.java
@@ -1,16 +1,24 @@
 package mcjty.rftoolsbuilder.setup;
 
 
+import mcjty.rftoolsbase.RFToolsBase;
 import mcjty.rftoolsbuilder.RFToolsBuilder;
+import mcjty.rftoolsbuilder.modules.builder.BuilderModule;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import static mcjty.rftoolsbuilder.RFToolsBuilder.MODID;
 
@@ -22,6 +30,7 @@ public class Registration {
     public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, MODID);
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RFToolsBase.MODID);
 
     public static void register() {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
@@ -30,10 +39,20 @@ public class Registration {
         CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        TABS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
 
     public static Item.Properties createStandardProperties() {
         return RFToolsBuilder.setup.defaultProperties();
     }
+
+    public static RegistryObject<CreativeModeTab> TAB = TABS.register("rftoolsbuilder", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + MODID))
+            .icon(() -> new ItemStack(BuilderModule.BUILDER.get()))
+            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+            .displayItems((featureFlags, output) -> {
+                RFToolsBuilder.setup.populateTab(output);
+            })
+            .build());
 }
